3ad0c9bf630400cef3d54e8e64bd16d3d85a081a
Ondrej Javorsky <gikol113@gmail.com>
2022-12-02 19:16:37 +0100

    -

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,62 @@
+import {type} from "os";
+
 class Triple<T> {
-    readonly #data: Array<T> = new Array(3)
+    readonly #data: Array<T> = new Array(3);
 
     constructor(left: T, middle: T, right: T) {
-        this.#data[0] = left
-        this.#data[1] = middle
-        this.#data[2] = right
+        this.#data[0] = left;
+        this.#data[1] = middle;
+        this.#data[2] = right;
     }
 
-    data(index: number): T {
+    data(index: Triple.Index | number): T {
         if (index >= 0 && index <= 2) {
-            return this.#data[index]
+            return this.#data[index];
         } else {
-            throw "Index out of range!"
+            throw "Index out of range!";
         }
+    }
+
+    isFull(): boolean {
+        return this.#data[0] != null && this.#data[1] !== null && this.#data[2] !== null;
+    }
+
+    isValue(): boolean {
+        return typeof this.#data[0] !== "object";
+    }
+
+    isReference(): boolean {
+        return typeof this.#data[0] === "object";
     }
 }
 
-const triple = new Triple<number>(10, 5, 2);
+module Triple {
+    export enum Index {
+        Left = 0,
+        Middle = 1,
+        Right = 2
+    }
+}
+
+const triple = new Triple<Triple<number>>(new Triple(5, 15 ,29), new Triple(5, 15 ,29), new Triple(5, 15 ,29));
+console.log(triple.isFull())
 console.log(triple.data(2))
+
+class PerstArray<T> {
+    #root: Triple<Triple<T>> = null;
+
+    constructor(size: number) {
+        // implement
+        console.log(this.#root)
+    }
+
+    #insert(root: Triple<any>, value: T) {
+        if (!root) {
+            this.#root = new Triple(new Triple<T>(value, null, null), null, null)
+        } else {
+            if (root.isReference()) {
+
+            }
+        }
+    }
+}
